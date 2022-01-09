1bd6a070f5c790255f7f52a3806b9e40d88a4900
Jumgalbek Arymbaev <Jumgalbek.Arymbaev@ulta.com>
2022-01-09 11:18:17 -0600

    code before connecting to DB by using TypeORM

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,46 @@
+import { Body, Controller, Get, Post, Param, Delete, Patch, Query } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto'
+import { UpdateTaskStatusDto } from './dto/update-task.dto';
+import { Task, TaskStatus } from './task.model';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+    constructor(private taskService: TasksService) {
+
+    }
+
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.taskService.getTaskById(id)
+    }
+
+    @Get()
+    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+        if (Object.keys(filterDto).length) {
+            return this.taskService.getTaskWithFilters(filterDto)
+        } else {
+            return this.taskService.getAllTasks()
+        }
+    }
+
+    @Post()
+    createTask(@Body() createTaskDto: CreateTaskDto
+    ): Task {
+        return this.taskService.createTask(createTaskDto)
+    }
+
+    @Delete('/:id')
+    deleteTaskById(@Param('id') id: string): void {
+        return this.taskService.deleteTaskById(id)
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(@Param('id') id: string,
+        @Body() updateTaskStatusDto: UpdateTaskStatusDto
+    ): Task {
+        const { status } = updateTaskStatusDto
+        return this.taskService.updateTaskStatus(id, status)
+    }
+}
